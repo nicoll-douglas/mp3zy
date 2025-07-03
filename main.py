ad5e2feae6b3888828f5bab0f56bf1b638d4a14c
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-07-03 11:45:05 +0100

    update

--- main.py
+++ main.py
@@ -12,21 +12,24 @@ from core import Sync
 import time
 
 def main():
-  logging.info(f"🟡 NEW SYNC STARTED AT {datetime.now()} 🟡")
-  spotify_client = SpotifyApiClient()
-  ftp_music_manager = MusicManager()
-  spotify_sync = Sync(spotify_client, ftp_music_manager)
-  spotify_sync.trigger()
-  logging.info(f"🟢 SYNC FINISHED AT {datetime.now()} 🟢")
-  ftp_music_manager.quit()
-
-try:
   while True:
-    main()
+    try:
+      logging.info(f"🟡 NEW SYNC STARTED AT {datetime.now()} 🟡")
+      
+      spotify_client = SpotifyApiClient()
+      ftp_music_manager = MusicManager()
+      spotify_sync = Sync(spotify_client, ftp_music_manager)
+      spotify_sync.trigger()
+      
+      logging.info(f"🟢 SYNC FINISHED AT {datetime.now()} 🟢")
+      
+      ftp_music_manager.quit()
+    except Exception as e:
+      logging.critical(e)
+      logging.critical("Failed to sync.")
+
     sync_interval = os.getenv("SYNC_INTERVAL") or 12
     logging.info(f"🔴 SLEEPING FOR {sync_interval} HOURS BEFORE NEXT SYNC 🔴")
     time.sleep(int(sync_interval) * 60 * 60)
-except Exception as e:
-  logging.critical(e)
-  logging.critical("Failed to sync.")
-  sys.exit(1)
+
+main()
