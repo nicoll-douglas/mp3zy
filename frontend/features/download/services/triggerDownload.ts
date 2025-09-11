c188255b9c1333bfab60b43d13a150d02d44ff69
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-09-11 14:58:44 +0100

    commit mesage

--- frontend/features/download/services/triggerDownload.ts
+++ frontend/features/download/services/triggerDownload.ts
@@ -5,22 +5,21 @@ export default async function triggerDownload(
   audioUrl: string | null,
   data: DownloadOptionsFormValues
 ): Promise<{ taskId: string }> {
-  // const headers = getBackendAuthHeaders();
-  // const requestUrl = `${import.meta.env.VITE_BACKEND_URL}/download`;
+  const headers = getBackendAuthHeaders();
+  const requestUrl = `${import.meta.env.VITE_BACKEND_URL}/download`;
 
-  // const res = await fetch(requestUrl, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     ...headers,
-  //   },
-  //   body: JSON.stringify({
-  //     ...data,
-  //     downloadUrl: audioUrl,
-  //     artists: data.artists.map((a) => a.value),
-  //   }),
-  // });
-  // const body = await res.json();
-  // return body;
-  return { taskId: "1" };
+  const res = await fetch(requestUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    body: JSON.stringify({
+      ...data,
+      downloadUrl: audioUrl,
+      artists: data.artists.map((a) => a.value),
+    }),
+  });
+  const body = await res.json();
+  return body;
 }
