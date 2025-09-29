37ab65f71af391771d2f920e508e14b73cceb369
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-09-29 14:58:28 +0100

    add __init__.py for types

---
+++ backend/types/__init__.py
@@ -0,0 +1,7 @@
+from .artist_names_metadata import ArtistNamesMetadata
+from .download_search_result import DownloadSearchResult
+from .download_status import DownloadStatus
+from .download_update import DownloadUpdate
+from .track_bitrate import TrackBitrate
+from .track_codec import TrackCodec
+from .track_release_date import TrackReleaseDate
