4882ec37ab9e2dcfbd8dcc95fcab63bb4b394978
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-10-03 19:10:29 +0100

    add docstrings

--- backend/user_types/reponses/post_downloads_response.py
+++ backend/user_types/reponses/post_downloads_response.py
@@ -1,7 +1,18 @@
 class PostDownloadsResponse:
+  """Class that contains nested classes to be used as models for responses to the POST /downloads endpoint.
+  """
+
   class BadRequest:
+    """Represents the response body for a 400 status code response to a POST /downloads request.
+
+    Attributes:
+      field (str): The first field that failed request validation; will match a key in the JSON request body.
+      message (str): A user-friendly message indicating the validation error.
+    """
+    
     field: str
     message: str
+
 
     def __init__(self, field: str, message: str):
       self.field = field
@@ -9,12 +20,22 @@ class PostDownloadsResponse:
     # END __init__
   # END class BadRequest
 
+
   class Ok:
+    """Represents the response body for a 200 status code response to a POST /downloads request.
+
+    Attributes:
+      download_id (int): The database ID of the download that was freshly queued and inserted into the database.
+      message (str): A user-friendly message.
+    """
+
     download_id: int
     message: str = "Your download has been queued and should start shortly."
+
 
     def __init__(self, download_id: int):
       self.download_id = download_id
     # END __init__
+  # END class Ok
 
 # END class PostDownloadsResponse
--- backend/user_types/requests/post_downloads_request.py
+++ backend/user_types/requests/post_downloads_request.py
@@ -4,6 +4,20 @@ from ..track_bitrate import TrackBitrate
 from ..track_release_date import TrackReleaseDate
 
 class PostDownloadsRequest:
+  """Type that represents a validated request body to endpoint POST /downloads.
+
+  Attributes:
+    artist_names (TrackArtistNames): Track artist metadata.
+    track_name (str): The name of the track.
+    album_name (str): Album name metadata.
+    codec (TrackCodec): The audio codec to use when saving the track file to disk.
+    bitrate (TrackBitrate): The bitrate to use when saving an associated audio file to disk.
+    track_number (int | None): Track number metadata.
+    disc_number (int | None): Disc number metadata.
+    release_date (TrackReleaseDate | None): Track release date metadata.
+    url (str): The URL to use as the audio source for the track download.
+  """
+  
   artist_names: TrackArtistNames
   track_name: str
   album_name: str | None
@@ -13,4 +27,4 @@ class PostDownloadsRequest:
   disc_number: int | None
   release_date: TrackReleaseDate | None
   url: str
-# END post_downloads_request
+# END class PostDownloadsRequest
