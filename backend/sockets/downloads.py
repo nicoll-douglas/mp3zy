268c806b7c8a0b792a6abdb050200516dda88cbe
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-10-06 19:31:37 +0100

    add docstr

--- backend/sockets/downloads.py
+++ backend/sockets/downloads.py
@@ -4,9 +4,16 @@ from user_types import DownloadUpdate
 DOWNLOADS_NAMESPACE = "/downloads"
 
 class DownloadsSocket(Namespace):
+  """Socket namespace that deals with downloads.
+
+  Attributes:
+    DOWNLOAD_UPDATE_EVENT (str): The name of the event for download upates.
+    DOWNLOAD_INIT_EVENT (str): The name of the event for sending all downloads (downloads initialization).
+  """
 
   DOWNLOAD_UPDATE_EVENT = "download_update"
   DOWNLOAD_INIT_EVENT = "download_init"
+
 
   def on_connect(self):
     pass
