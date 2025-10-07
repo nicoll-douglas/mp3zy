ba5593bc1751d824322d2f825ea14c1c28df2441
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-10-07 21:44:06 +0100

    add fixture

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -17,3 +17,17 @@ def in_memory_db_conn() -> Generator[sqlite3.Connection, None, None]:
 
   conn.close()
 # END in_memory_db_conn
+
+
+@pytest.fixture
+def app_db(in_memory_db_connn: sqlite3.Connection) -> Generator[sqlite3.Connection, None, None]:
+  """Fixture that provides a database connection for an in-memory SQLite database that is set up with the application's schema.
+  
+  Returns:
+    Generator[sqlite3.Connection, None, None]: Yields the database connection.
+  """
+
+  db.setup(in_memory_db_connn)
+
+  yield in_memory_db_connn
+# END app_db
